40e791e6982510b16bd8e600e6735daec13f5b96
JDavid S <jdavid.0794.sv@gmail.com>
2022-12-02 17:01:35 -0500

    tema | - en curso

---
+++ conceptos-basicos/numbers.rb
@@ -0,0 +1,37 @@
+#Adition
+1 + 1 #=> 2
+#Subtraction
+2 - 1 #=> 1
+#Multiplication
+2 * 2 #=> 4
+#División
+2 / 2 #=> 1
+#Exponent
+2 ** 2 #=> 4
+3 ** 4 #=> 81
+#Modules (find the remainder of the division)
+8 % 2 #=> 0 (8 / 2 = 4; no remainder)
+10 % 4 #=> 2 (10 / 4 = 2 with a remainder of 2)
+
+#Integers and floats
+17 / 5 #=> 3, not 3.4
+17 / 5.0 #=> 3.4
+
+#Converting Number Types
+#To convert an integer to float
+13.to_f #=> 13.0
+#To convert an float to integer
+13.0.to_i #=> 13
+13.9.to_i #=> 13
+
+#Some Useful Number Methods
+#even?
+6.even? #=> true
+7.even? #=> false
+
+#odd?
+6.odd? #=> false
+7.odd? #=> true
+
+
+
---
+++ conceptos-basicos/strings.rb
@@ -0,0 +1,43 @@
+#Strings
+#Concatenation
+
+# With the plus operator:
+"Welcome " + "to " + "Odin!"    #=> "Welcome to Odin!"
+
+# With the shovel operator:
+"Welcome " << "to " << "Odin!"  #=> "Welcome to Odin!"
+
+# With the concat method:
+"Welcome ".concat("to ").concat("Odin!")  #=> "Welcome to Odin!"
+
+#Substrings
+
+"hello"[0]      #=> "h"
+
+"hello"[0..1]   #=> "he"
+
+"hello"[0, 4]   #=> "hell"
+
+"hello"[-1]     #=> "o"
+
+#Escape characters
+
+=begin 
+\\  #=> Need a backslash in your string?
+\b  #=> Backspace
+\r  #=> Carriage return, for those of you that love typewriters
+\n  #=> Newline. You'll likely use this one the most.
+\s  #=> Space
+\t  #=> Tab
+\"  #=> Double quotation mark
+\'  #=> Single quotation mark 
+=end
+
+irb(main):001:0> puts "Hello \n\nHello"
+Hello
+
+Hello
+=> nil
+
+
+
